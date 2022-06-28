e4ef81123b4118c8cc0d3daa20a70e0746e33750
Lucas Souza <lucassouza.1000@hotmail.com>
2022-06-27 21:09:41 -0300

    add images

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
-
 const app = express()
 
 app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
 
 app.get('/', (request, response) => {
   response.render('home', {
